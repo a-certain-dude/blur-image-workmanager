97bd544350b6ddc86db6c1a593beb3af2b675457
Jose <nkwantabisajoseph25@gmail.com>
2025-07-05 12:23:06 +0000

    save file to mediaStore

--- app/src/main/java/com/example/bluromatic/workers/SaveImageToFileWorker.kt
+++ app/src/main/java/com/example/bluromatic/workers/SaveImageToFileWorker.kt
@@ -10,7 +10,42 @@ class SaveImageToFileWorker(ctx: Context, params: WorkerParameters) : CoroutineW
     private val title = "blurred image"
     private val dateFormater = SimpleDateFormat("yyyy.MM.dd 'at' HH:mm:ss Z ", Locale.getDefault())
     override suspend fun doWork(): Result {
-        return
+        makeStatusNotification(
+            message = applicationContext.resources.getString(R.string.saving_image),
+            context = applicationContext
+        )
+        
+        return withContext(context = Dispatchers.IO) {
+            delay(timeMillis = DELAY_TIME_MILLIS)
+            val resolver = applicationContext.contentResolver
+            return@withContext try {
+                val resourceUri =
+                    inputData.getString(KEY_IMAGE_URI) /* input data ---- temporarily blurred image URI */
+                val bitmap =
+                    BitmapFactory.decodeStream(resolver.openInputStream(Uri.parse(resourceUri)))
+                val imageUrl = MediaStore.Images.Media.insertImage(
+                    resolver, bitmap, title, dateFormater.format(
+                        Date()
+                    )
+                )
+                /* if (!imageUrl.isNotEmpty()) */
+                if (imageUrl.isEmpty()){
+                    val output = workDataOf(KEY_IMAGE_URI to imageUrl)
+                    Result.success(output)
+                }else{
+                    Log.e(TAG,applicationContext.resources.getString(R.string.writing_to_mediaStore_failed))
+                    Result.failure()
+                }
+                
+                Result.success()
+            } catch (exception: Exception) {
+                Log.e(TAG,applicationContext.resources.getString(R.string.error_saving_image),exception)
+                Result.failure()
+                
+            }
+        }
+        
+        
     }
     
     
